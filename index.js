931dd0166347eb1445d82ece9259eb670c118394
Arghya20 <arghyaghosh.vlog@gmail.com>
2022-12-02 03:29:04 +0530

    mongodb intrigrated

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.s14y4qr.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 app.get("/", async (req, res) => {
   res.send("Our Server is Running 🙌");
