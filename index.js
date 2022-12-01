e4a6a1d0f8b09df187371c96987b569b1bef6323
Arghya20 <arghyaghosh.vlog@gmail.com>
2022-12-02 03:38:46 +0530

    all products data loded

--- index.js
+++ index.js
@@ -9,14 +9,27 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.s14y4qr.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
+
+async function run() {
+  try {
+    const productsCollection = client.db("GUIDANCE-CAR").collection("products");
+
+    app.get("/products", async (req, res) => {
+      const query = {};
+      const cursor = productsCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 app.get("/", async (req, res) => {
   res.send("Our Server is Running 🙌");
